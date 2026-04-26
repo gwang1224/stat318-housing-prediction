ee305f49b1bbfae4591a9247d4b7295323dd74e3
gwang1224 <gracewang187@gmail.com>
2026-04-25 22:14:42 -0400

    train test data

---
+++ train_test_dataset.R
@@ -0,0 +1,11 @@
+data = read.csv("/Users/gracewang/stat318-housing-prediction/data/cleaned_ames.csv")
+
+train_index = sample(1:nrow(data), 2341, replace=FALSE )
+
+data.train = data[train_index,]
+write.csv(data.train, "/Users/gracewang/stat318-housing-prediction/data/ames_train.csv")
+
+data.test = data[-train_index,]
+write.csv(data.train, "/Users/gracewang/stat318-housing-prediction/data/ames_test.csv")
+
+
