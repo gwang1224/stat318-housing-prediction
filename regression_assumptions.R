eca0ee3e88005a4e125ff786de33142272efe67f
gwang1224 <gracewang187@gmail.com>
2026-04-29 18:13:11 -0400

    assupmtions

--- regression_assumptions.R
+++ regression_assumptions.R
@@ -1,44 +1,27 @@
 # ------------------------------------
-# Regression Assumptions for AIC Model
+# Regression Assumptions for BIC Model
 # ------------------------------------
-ames.train = read.csv("/Users/gracewang/stat318-housing-prediction/data/ames_train.csv", header=TRUE)
-ames.test = read.csv("/Users/gracewang/stat318-housing-prediction/data/ames_test.csv", header=TRUE)
+ames.train = read.csv("/Users/gracewang/stat318-housing-prediction/data/ames_fully_cleaned.csv", header=TRUE)
 
-bic.model = lm(SalePrice ~ Lot.Area + Street + Land.Slope + Neighborhood + Condition.2 + 
-                 Bldg.Type + Overall.Qual + Overall.Cond + Year.Built + Year.Remod.Add + 
-                 Roof.Matl + Mas.Vnr.Area + Exter.Qual + Bsmt.Qual + Total.Bsmt.SF + 
-                 Gr.Liv.Area + Bsmt.Full.Bath + Bedroom.AbvGr + Kitchen.AbvGr + 
-                 Kitchen.Qual + Functional + Fireplaces + Garage.Area + Wood.Deck.SF + 
-                 Screen.Porch + Pool.QC + Misc.Feature + Sale.Condition, data=ames.train)
+bic.model = lm(SalePrice ~ Lot.Area + Lot.Config + Land.Slope + Neighborhood + 
+                 Condition.1 + Bldg.Type + House.Style + Overall.Qual + Overall.Cond + 
+                 Year.Built + Mas.Vnr.Type + Mas.Vnr.Area + Exter.Qual + Bsmt.Qual + 
+                 Gr.Liv.Area + Bsmt.Full.Bath + Full.Bath + Bedroom.AbvGr + 
+                 Kitchen.Qual + Functional + Fireplaces + Garage.Cars + Wood.Deck.SF + 
+                 Screen.Porch + Misc.Val + Sale.Condition, data=ames.train)
 
-summary(bic.model) #adj r^2 = 0.9135
+summary(bic.model) #adj r^2 = 0.888
 
 
 # Residual plot
-
 plot(resid(bic.model)~fitted(bic.model), xlab="Y.hat", ylab="Residuals")
 abline(h=0, col="red")
-
-library(MASS)
-bc = boxcox(bic.model)
-bc$x[which.max(bc$y)] #0.30
-
-new.bic.model = lm(SalePrice^0.3 ~ Lot.Area + Street + Land.Slope + Neighborhood + Condition.2 + 
-                 Bldg.Type + Overall.Qual + Overall.Cond + Year.Built + Year.Remod.Add + 
-                 Roof.Matl + Mas.Vnr.Area + Exter.Qual + Bsmt.Qual + Total.Bsmt.SF + 
-                 Gr.Liv.Area + Bsmt.Full.Bath + Bedroom.AbvGr + Kitchen.AbvGr + 
-                 Kitchen.Qual + Functional + Fireplaces + Garage.Area + Wood.Deck.SF + 
-                 Screen.Porch + Pool.QC + Misc.Feature + Sale.Condition, data=ames.train)
-
-summary(new.bic.model) #Adj R^2 = 0.9255
-
-plot(resid(new.bic.model) ~ fitted(new.bic.model), xlab="Y.hat", ylab="Residuals")
-abline(h=0, col="red")
-
 
 # Normality
 qqnorm(resid(new.bic.model))
 qqline(resid(new.bic.model), col = "red")
+
+
 
 # Outliers
 cooks <- cooks.distance(new.bic.model)
@@ -54,23 +37,39 @@ plot(
   xlab = "Observation Index"
 )
 
-
-ames.train.clean <- ames.train[-2275, ]
+ames.train.clean <- ames.train[-c(1931, 1333, 1673), ]
 
 new.bic.model.clean <- update(
-  new.bic.model,
+  bic.model,
   data = ames.train.clean
 )
 
-summary(new.bic.model.clean) #Adj R^2 = 0.9318
-anova(new.bic.model.clean)
+summary(new.bic.model.clean) #Adj R^2 = 0.9148
 
-# Resid plot
 plot(resid(new.bic.model.clean)~fitted(new.bic.model.clean), xlab="Y.hat", ylab="Residuals")
 abline(h=0, col="red")
 
+
+
+library(MASS)
+bc = boxcox(new.bic.model.clean)
+bc$x[which.max(bc$y)] #0.26
+
+new.bic.model = lm(SalePrice^0.26 ~ Lot.Area + Lot.Config + Land.Slope + Neighborhood + 
+                     Condition.1 + Bldg.Type + House.Style + Overall.Qual + Overall.Cond + 
+                     Year.Built + Mas.Vnr.Type + Mas.Vnr.Area + Exter.Qual + Bsmt.Qual + 
+                     Gr.Liv.Area + Bsmt.Full.Bath + Full.Bath + Bedroom.AbvGr + 
+                     Kitchen.Qual + Functional + Fireplaces + Garage.Cars + Wood.Deck.SF + 
+                     Screen.Porch + Misc.Val + Sale.Condition, data=ames.train.clean)
+
+summary(new.bic.model)
+
+# Resid plot
+plot(resid(new.bic.model)~fitted(new.bic.model), xlab="Y.hat", ylab="Residuals")
+abline(h=0, col="red")
+
 # FINAL
-qqnorm(resid(new.bic.model.clean))
+qqnorm(resid(new.bic.model))
 qqline(resid(new.bic.model), col = "red")
 
 
