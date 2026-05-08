d266fb234c00e925e89c7163050c1765f1c5d53e
sday04 <syld.9504@gmail.com>
2026-05-07 21:40:47 -0400

    Update 5_regression_assumptions.R

--- 5_regression_assumptions.R
+++ 5_regression_assumptions.R
@@ -1,7 +1,7 @@
 # ------------------------------------
 # Regression Assumptions for BIC Model
 # ------------------------------------
-ames.train = read.csv("/Users/gracewang/stat318-housing-prediction/data/ames_fully_cleaned.csv", header=TRUE)
+ames.train = read.csv("~/Courses/STAT 318/Project/Data/ames_fully_cleaned.csv", header=TRUE)
 
 bic.model = lm(SalePrice ~ Lot.Area + Lot.Config + Land.Slope + Neighborhood + 
                  Condition.1 + Bldg.Type + House.Style + Overall.Qual + Overall.Cond + 
@@ -9,11 +9,11 @@ bic.model = lm(SalePrice ~ Lot.Area + Lot.Config + Land.Slope + Neighborhood +
                  Bedroom.AbvGr + Kitchen.Qual + Functional + Garage.Cars + 
                  Wood.Deck.SF + Screen.Porch + Misc.Val + Sale.Condition, data=ames.train)
 
-summary(bic.model) #adj r^2 = 0.885
+summary(bic.model) #adj r^2 = 0.888
 
 
 # Residual plot
-plot(resid(bic.model)~fitted(bic.model), xlab="Y.hat", ylab="Residuals")
+plot(resid(bic.model)~fitted(bic.model), xlab="Y.hat", ylab="Residuals", main = "Residual Plot")
 abline(h=0, col="red")
 
 # Normality
@@ -42,14 +42,14 @@ plot(
 )
 abline(h=cutoff, col="red")
 
-ames.train.clean <- ames.train[-c(1931, 1333, 1673), ]
+ames.train.clean <- ames.train[-c(1931, 1333), ]
 
 new.bic.model.clean <- update(
   bic.model,
   data = ames.train.clean
 )
 
-summary(new.bic.model.clean) #Adj R^2 = 0.9148
+summary(new.bic.model.clean) #Adj R^2 = 0.911
 
 plot(resid(new.bic.model.clean)~fitted(new.bic.model.clean), xlab="Y.hat", ylab="Residuals")
 abline(h=0, col="red")
@@ -62,13 +62,11 @@ bc$x[which.max(bc$y)] #0.26
 
 new.bic.model = lm(SalePrice^0.26 ~ Lot.Area + Lot.Config + Land.Slope + Neighborhood + 
                      Condition.1 + Bldg.Type + House.Style + Overall.Qual + Overall.Cond + 
-                     Year.Built + Mas.Vnr.Type + Mas.Vnr.Area + Exter.Qual + Bsmt.Qual + 
-                     Gr.Liv.Area + Bsmt.Full.Bath + Full.Bath + Bedroom.AbvGr + 
-                     Kitchen.Qual + Functional + Fireplaces + Garage.Cars + Wood.Deck.SF + 
-                     Screen.Porch + Misc.Val + Sale.Condition, data=ames.train.clean)
+                     Year.Built + Exter.Qual + Bsmt.Qual + Gr.Liv.Area + Bsmt.Full.Bath + 
+                     Bedroom.AbvGr + Kitchen.Qual + Functional + Garage.Cars + 
+                     Wood.Deck.SF + Screen.Porch + Misc.Val + Sale.Condition, data=ames.train.clean)
 
 summary(new.bic.model)
-anova(new.bic.model)
 
 # Resid plot
 plot(resid(new.bic.model)~fitted(new.bic.model), xlab="Y.hat", ylab="Residuals")
@@ -77,5 +75,7 @@ abline(h=0, col="red")
 # FINAL
 qqnorm(resid(new.bic.model))
 qqline(resid(new.bic.model), col = "red")
+
+
 
 
